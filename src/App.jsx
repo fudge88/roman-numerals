6c40c28f100083266aa6bc0024fd2650ea0e0774
fahra akhlaq <f.akhlaq@outlook.com>
2025-03-12 19:52:27 +0000

    dependencies added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Typography from '@mui/joy/Typography';
 import './App.css';
 
 function App() {
-	return <>test</>;
+	return (
+		<>
+			test
+			<Typography color="danger" level="h1">
+				joy ui test
+			</Typography>
+		</>
+	);
 }
 
 export default App;
